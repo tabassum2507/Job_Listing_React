4c16a94667b50a8da2cd82ac44a8810ba81b4c31
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2024-06-26 00:10:24 +0530

    components added

---
+++ src/components/Cards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cards = ({children, bg = 'bg-gray-100'}) => {
+  return (
+    <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>
+  )
+}
+
+export default Cards
---
+++ src/components/JobListings.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import jobs from '../jobs.json'
+import JobListing from "./JobListing";
+
+const JobListings = () => {
+  const [allJobs, setAllJobs] = useState([]);
+
+  useEffect(() => {
+    if (Array.isArray(jobs.jobs)) {
+      setAllJobs(jobs.jobs);
+    } else {
+      console.error('Jobs data is not an array:', jobs.jobs);
+    }
+  }, []);
+ 
+  return (
+    <div>
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            Browse Jobs
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+         { allJobs?.map((job) => ( 
+          <JobListing key={job.id} job={job} />
+          ) )} 
+            
+           
+       
+           
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default JobListings;
